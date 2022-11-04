fca85a5344e1cac8042e986faacdc82dab01fce3
Ida Lindgren <ida.nacka@gmail.com>
2022-11-04 16:41:30 +0100

    ew

--- src/main.js
+++ src/main.js
@@ -1,28 +1,33 @@
+
   const todoForm = document.querySelector(".todo-form")
   const todoInput = document.querySelector(".todo-input")
   const todoItemsList = document.querySelector(".todo-items")
   
+  
   let todos =[];
 
+  //lyssnar efter submit
   todoForm.addEventListener("submit", 
   function(event){
     event.preventDefault();
     addTodo(todoInput.value);
-  }
-  );
+  });
 
+  //funktion som lägger till todo
   function addTodo(item){
     if (item !== ""){
+        //todo objekt 
         const todo ={
             id: Date.now(),
             name: item,
             completed: false
         };
 
+        //lägger till todos till listan
         todos.push(todo);
-        addToLocalStorage(todos);
+        addToLocalStorage(todos); //sparas
 
-        todoInput.value ="";
+        todoInput.value =""; //bort med dig
     }
   }
 
@@ -31,10 +36,10 @@
     todos.forEach(function(item){
         const checked = item.completed ? "checked" : null;
         const li = document.createElement("li");
-        li.setAttribute("class", "item");
+        li.setAttribute("class", "item"); //<li class="item"></li>
         li.setAttribute("data-key", item.id);  
 
-        if(item.completed == true) {
+        if(item.completed === true) {
             li.class.add("checked");
         }
 
@@ -44,23 +49,24 @@
         <button class="delete-button">X</button>
         `;
 
-        todoItemsList.append(li);
+        todoItemsList.append(li); // lägger till <li> till <ul>
     });    
   }
+  //funktion för local storage
   function addToLocalStorage(todos){
     localStorage.setItem("todos", JSON.stringify(todos));
     renderTodos(todos);
   }
-  function getFormLocalStorage(){
+  function getFromLocalStorage(){
     const reference = localStorage.getItem("todos");
-    if(reference){
+    if(reference){ //konverterar
         todos = JSON.parse(reference);
         renderTodos(todos);
     }
   }
   function toggle(id){
     todos.forEach(function(item){
-        if(item.id == id){
+        if(item.id == id){ //olika datatyper 
             item.completed = !item.completed;
         }
     });
@@ -76,7 +82,7 @@ function deleteTodo(id){
     addToLocalStorage();
 }
 
-getFormLocalStorage();
+getFromLocalStorage();
 
 todoItemsList.addEventListener("click", function(event){
     if(event.target.type === "checkbox"){
@@ -87,7 +93,3 @@ todoItemsList.addEventListener("click", function(event){
         deleteTodo(event.target.parentElement.getAttribute("data-key"));
     }
 });
-
-
-
-
